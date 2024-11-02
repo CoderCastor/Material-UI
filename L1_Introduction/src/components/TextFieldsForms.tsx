ea0dee9faeb05927f730ba4d8179ac83ea6dccd5
codercastor <tejasshinde935@gmail.com>
2024-11-02 21:01:00 +0530

    saved

--- L1_Introduction/src/components/TextFieldsForms.tsx
+++ L1_Introduction/src/components/TextFieldsForms.tsx
@@ -1,9 +1,11 @@
 import { InputAdornment, Stack, TextField } from "@mui/material";
-// import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { useState } from "react";
 
 function TextFieldsForms() {
   const [value, setValue] = useState("");
+  const [visibile,setVisible] = useState(false)
 
   return (
     <Stack spacing={4}>
@@ -70,9 +72,9 @@ function TextFieldsForms() {
         />
       </Stack>
       <Stack spacing={2} direction={"row"}>
-        {/* <TextField label="Password" type='password' color='success' InputProps={{
-          endAdornment: <VisibilityIcon position='end'/>
-        }} /> */}
+        <TextField label="Password" type={!visibile ? 'password' : 'text'} color='success' InputProps={{
+          endAdornment: !visibile ? (<InputAdornment position="end" className="cursor-pointer"><VisibilityIcon onClick={()=>setVisible(true)} fontSize="small"/></InputAdornment>) : (<InputAdornment position="end"><VisibilityOffIcon className="cursor-pointer" onClick={()=>setVisible(false)} fontSize="small" /></InputAdornment>)
+        }} />
       </Stack>
     </Stack>
   );
