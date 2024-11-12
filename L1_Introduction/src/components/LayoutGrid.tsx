29946f2c1ba885856bf127c67d3d3591d8060c1f
codercastor <tejasshinde935@gmail.com>
2024-11-13 00:03:46 +0530

    Grid implemented

--- L1_Introduction/src/components/LayoutGrid.tsx
+++ L1_Introduction/src/components/LayoutGrid.tsx
@@ -1,19 +1,29 @@
 import { Box, Grid, Grid2 } from "@mui/material";
 
 function LayoutGrid() {
+    //only xs distributed equal width to all boxes
+    //there rowSpacing and column spacing on GRID
   return (
-    <Grid container ml={4}>
-      <Grid item xs={12} sm={6}>
-        <Box bgcolor={"primary.light"} p={4}>Item 1</Box>
+    <Grid container ml={4} spacing={2}>
+      <Grid item xs sm={6}>
+        <Box bgcolor={"primary.light"} p={4}>
+          Item 1
+        </Box>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <Box bgcolor={"primary.light"} p={4}>Item 2</Box>
+      <Grid item xs sm={6}>
+        <Box bgcolor={"primary.light"} p={4}>
+          Item 2
+        </Box>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <Box bgcolor={"primary.light"} p={4}>Item 3</Box>
+      <Grid item xs sm={6}>
+        <Box bgcolor={"primary.light"} p={4}>
+          Item 3
+        </Box>
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <Box bgcolor={"primary.light"} p={4}>Item 4</Box>
+      <Grid item xs sm={6}>
+        <Box bgcolor={"primary.light"} p={4}>
+          Item 4
+        </Box>
       </Grid>
       {/* <Grid2></Grid2> */}
     </Grid>
